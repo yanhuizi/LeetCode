a7656efa53dcd3b8e01d2ba998478a015dd5fbe3
yan_huizi <3072413643@qq.com>
2026-07-30 22:18:45 +0800

    长度最小的子数组

---
+++ Array/209.长度最小的子数组.c
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=c
+ *
+ * [209] 长度最小的子数组
+ */
+
+// @lc code=start
+int minSubArrayLen(int target, int* nums, int numsSize) {
+    int left = 0;
+    int sum = 0;
+    int minLen = numsSize + 1;
+
+    for (int right = 0; right < numsSize; right++) {
+        sum += nums[right];
+
+        while (sum >= target) {
+            int len = right - left + 1;
+            if (len < minLen) {
+                minLen = len;
+            }
+            sum -= nums[left];
+            left++;
+        }
+    }
+
+    if (minLen == numsSize + 1) {
+        return 0;
+    }else{
+        return minLen;
+    }
+
+}
+// @lc code=end
+
