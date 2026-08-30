2df9a017f1d1f2d980872c9878b1fd9415a9695b
yan_huizi <3072413643@qq.com>
2026-08-30 21:55:07 +0800

    双指针法和暴力破解法

--- Array/27.移除元素.c
+++ Array/27.移除元素.c
@@ -8,12 +8,15 @@
 int removeElement(int* nums, int numsSize, int val) {
     int k=0;
     for(int i=0;i<numsSize;i++){
-        if (nums[i]!=val){
-            nums[k]=nums[i];
-            k++;
+        if(nums[i]==val){
+            for(int j=i+1;j<numsSize;j++){
+                nums[j-1]=nums[j];//这里 不能写nums[j]=nums[j+1]
+            }
+            i--;//所有数值都往前移动了，所以下一个需要判断的数值还是第0项
+            numsSize--;
         }
     }
-    return k;
+    return numsSize;
 }
 // @lc code=end
 
