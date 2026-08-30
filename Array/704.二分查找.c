12585ee2aba6eb57f178d3f57a2b10e4aeb68463
yan_huizi <3072413643@qq.com>
2026-08-30 21:03:45 +0800

    重刷二分查找

--- Array/704.二分查找.c
+++ Array/704.二分查找.c
@@ -6,30 +6,19 @@
 
 // @lc code=start
 int search(int* nums, int numsSize, int target) {
-    //int* nums即传过来的nums这个数组第一个元素的地址
-    //约等于nums[]
     int left=0;
-    int right=numsSize-1;//定义的是左右边界的索引
-   
-    while(left<=right)
-    {
-        int mid=(left+right)/2;//中间元素的索引
-         if (target<nums[mid])
-        {
-            right=mid-1;
-        }
-        else if (target>nums[mid])
-        {
+    int right=numsSize-1;
+    while(left<=right){
+        int mid=(left+right)/2;//也可以写int mid = left + ((right - left)防止溢出
+        if(nums[mid]<target){
             left=mid+1;
-        }
-        else
-        {
+        }else if(nums[mid]>target){
+            right=mid-1;
+        }else{
             return mid;
         }
-       
     }
-    return -1;
-
+    return -1;//未找到目标值
 }
 // @lc code=end
 
