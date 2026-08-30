d4fbb8698a304dd0be73db885360aa76513cf2bf
yan_huizi <3072413643@qq.com>
2026-08-30 21:20:02 +0800

    重刷移除元素

--- Array/27.移除元素.c
+++ Array/27.移除元素.c
@@ -6,17 +6,12 @@
 
 // @lc code=start
 int removeElement(int* nums, int numsSize, int val) {
-    //循环判断数组的值是否为val，若为val则删除（？替换别的数值）并计数为k，否则保留。
-    //循环完毕返回不等于val值的数量k的大小以及新的数组
     int k=0;
-    for (int i = 0; i < numsSize; i++)
-    {
-        if (nums[i]!=val)
-        {
+    for(int i=0;i<numsSize;i++){
+        if (nums[i]!=val){
             nums[k]=nums[i];
             k++;
         }
-        
     }
     return k;
 }
