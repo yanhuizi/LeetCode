92b640ff065ff3b85f54e26b2a9fed1f37e9c221
yan_huizi <3072413643@qq.com>
2026-09-03 22:12:59 +0800

    重刷有序数组的平方

--- Array/977.有序数组的平方.c
+++ Array/977.有序数组的平方.c
@@ -9,25 +9,20 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* sortedSquares(int* nums, int numsSize, int* returnSize) {
-    int* arr=(int*)malloc(sizeof(int)*numsSize);
-    * returnSize=numsSize;
-
     int left=0;
-    int right=numsSize-1;//这里是索引所以-1
-    int pos=numsSize-1;//pos指当前要往新数组的哪个位置放数，也可以叫index
-    //左右两端一定有一个最大的数所以是从后往前开始填这个新的数组，所以最开始的pos放在最左边
+    int right=numsSize-1;
+    int pos=numsSize-1;
+    int *arr=(int*)malloc(sizeof(int)*numsSize);
+    *returnSize=numsSize;
 
-    while (right>=left)
-    {
-        int leftSquare=nums[left]*nums[left];
-        int rightSquare=nums[right]*nums[right];
-        if (rightSquare>leftSquare){
-            arr[pos]=rightSquare;
+    while(left<=right){
+        int leftNum=nums[left]*nums[left];
+        int rightNum=nums[right]*nums[right];
+        if(leftNum<rightNum){
+            arr[pos]=rightNum;
             right--;
-        }
-        else
-        {
-            arr[pos]=leftSquare;
+        }else{
+            arr[pos]=leftNum;
             left++;
         }
         pos--;
