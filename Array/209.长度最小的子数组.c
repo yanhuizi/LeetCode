fe7f12b0e08b3c7865dad04d1e7abbb2a0adc83d
yan_huizi <3072413643@qq.com>
2026-09-04 20:22:29 +0800

    重刷长度最小的数组

--- Array/209.长度最小的子数组.c
+++ Array/209.长度最小的子数组.c
@@ -6,24 +6,21 @@
 
 // @lc code=start
 int minSubArrayLen(int target, int* nums, int numsSize) {
-    int left = 0;
-    int sum = 0;
-    int minLen = numsSize + 1;
+    int left=0;
+    int sum=0;
+    int minLen=numsSize+1;
+    for(int right=0;right<numsSize;right++){
+        sum+=nums[right];
 
-    for (int right = 0; right < numsSize; right++) {
-        sum += nums[right];
-
-        while (sum >= target) {
-            int len = right - left + 1;
-            if (len < minLen) {
-                minLen = len;
+        while(sum>=target){
+            if((right-left+1)<minLen){
+                minLen=right-left+1;
             }
-            sum -= nums[left];
+            sum=sum-nums[left];
             left++;
         }
     }
-
-    if (minLen == numsSize + 1) {
+    if(minLen==numsSize+1){
         return 0;
     }else{
         return minLen;
