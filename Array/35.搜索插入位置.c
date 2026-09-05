6f9b109c6d96ac1c4f8abe03ed060bc79c2f82eb
yan_huizi <3072413643@qq.com>
2026-09-05 20:54:59 +0800

    重刷搜索插入位置

--- Array/35.搜索插入位置.c
+++ Array/35.搜索插入位置.c
@@ -8,13 +8,11 @@
 int searchInsert(int* nums, int numsSize, int target) {
     int left=0;
     int right=numsSize-1;
-
-    while (left<=right)
-    {
+    while(left<=right){
         int mid=(right+left)/2;
-        if (nums[mid]<target){
+        if(nums[mid]<target){
             left=mid+1;
-        }else if (nums[mid]>target){
+        }else if(nums[mid]>target){
             right=mid-1;
         }else{
             return mid;
